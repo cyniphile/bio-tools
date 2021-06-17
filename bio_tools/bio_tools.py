f485fafa18d1fa741cce81d39e889c32f51af7b1
Luke Schiefelbein <luke.schiefelbein@gmail.com>
2021-06-17 12:38:28 -0400

    update docstring

--- bio_tools/bio_tools.py
+++ bio_tools/bio_tools.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 
 def split_sequences_to_columns(sequences: pd.Series) -> pd.DataFrame:
+    """
+    TODO: rather slow, research jax unirep
+    """
     return sequences.apply(lambda x: pd.Series(list(x)))
